67b93b334408b00082f5acf979fcff60ba83110b
thepaperpilot <thepaperpilot@gmail.com>
2018-10-12 23:00:25 -0500

    Implemented transforming mechanic

---
+++ Assets/Scripts/TransformButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TransformButton : MonoBehaviour {
+
+    public GameObject transformationResult;
+
+    public void Transform() {
+        GameObject gameObject = Object.Instantiate(transformationResult);
+        Transform transform = TransformGun.currentTarget.transform;
+        gameObject.transform.SetPositionAndRotation(transform.position, transform.rotation);
+        Destroy(TransformGun.currentTarget);
+    }
+}
--- Assets/Scripts/TransformGun.cs
+++ Assets/Scripts/TransformGun.cs
@@ -1,7 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TransformGun : MonoBehaviour {
 
+    public static GameObject currentTarget;
+
+    public EventSystem eventSystem;
+    public GraphicRaycaster graphicRaycaster;
+    public float bulletTimeSpeed = 0.2f;
+    
     private void Update() {
         if (Input.GetMouseButtonDown(1)) {
             // We right clicked, lets see if we should open the transform menu
@@ -12,12 +22,33 @@ public class TransformGun : MonoBehaviour {
                 if (hit.transform.CompareTag("transformable")) {
                     // We hit something transformable
                     // Finally we can actually do something!
+                    currentTarget = hit.transform.gameObject;
                     TransformMenu.instance.gameObject.SetActive(true);
+                    Time.timeScale = bulletTimeSpeed;
                 }
             }
         }
         if (Input.GetMouseButtonUp(1)) {
+            // Find out which (if any) button we were on when we lifted up
+            PointerEventData data = new PointerEventData(eventSystem);
+            data.position = Input.mousePosition;
+            List<RaycastResult> results = new List<RaycastResult>();
+            //Raycast using the Graphics Raycaster and mouse click position
+            graphicRaycaster.Raycast(data, results);
+            results.Any(r => {
+                TransformButton button = r.gameObject.GetComponent<TransformButton>();
+                // If its one of our buttons, transform our object!
+                if (button) {
+                    button.Transform();
+                    return true;
+                }
+                return false;
+            });
+
+            // Reset everything
+            currentTarget = null;
             TransformMenu.instance.gameObject.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 }
