d0c88bd9ebc1c62cc25dd9550ee86ab84945e839
Hudson Bielstein <hbielstein@gmail.com>
2018-10-14 16:57:34 -0500

    Finished?

---
+++ Assets/ReturnToTitle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
