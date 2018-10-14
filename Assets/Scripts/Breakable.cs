8e18cef2964638633b4aaadbd7eb419507d01a03
thepaperpilot <thepaperpilot@gmail.com>
2018-10-14 00:45:01 -0500

    Made windows breakable

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Breakable : MonoBehaviour {
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer == 10) {
+            Destroy(gameObject.GetComponent<BoxCollider>());
+            Destroy(gameObject.GetComponent<MeshRenderer>());
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
