ad323eb8bfd75279f56c068fd48bfdb7dd1422b9
thepaperpilot <thepaperpilot@gmail.com>
2018-10-14 01:10:27 -0500

    Added pickup

---
+++ Assets/Scripts/GunPickup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class GunPickup : MonoBehaviour {
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.layer == 9) {
+            TransformGun.instance.gunAquired = true;
+            Destroy(gameObject.GetComponent<BoxCollider>());
+            Destroy(gameObject.GetComponent<MeshRenderer>());
+            AudioSource audio = gameObject.GetComponent<AudioSource>();
+            audio.Play();
+            Destroy(gameObject, audio.clip.length);
+        }
+    }
+}
