6aaa0889b3bcfd88ff473b1137495e1bbf09c08f
thepaperpilot <thepaperpilot@gmail.com>
2018-10-14 09:27:51 -0500

    Added title screen

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+    public void ChangeScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+}
